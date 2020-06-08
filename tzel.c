a4b9d5473c95811085c2b6740c5bda8395c5a552
Pedro Peck <pedrop.peck@gmail.com>
2020-06-08 09:24:55 +0300

    Rename file rootkit.c to tzel.c.

---
+++ tzel.c
@@ -0,0 +1,30 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/syscalls.h>
+#include <asm/unistd.h>
+#include <linux/sched.h>
+#include <linux/dirent.h>
+#include <linux/slab.h>
+#include <linux/version.h>
+#include <asm-generic/unistd.h>
+
+static unsigned long * get_syscall_table(void)
+{
+    
+}
+
+static int __init init_rootkit(void)
+{
+    printk(KERN_INFO "%p\n", get_syscall_table);
+
+    return 0;
+}
+
+static void __exit cleanup_rootkit(void)
+{
+    
+}  
+
+module_init(init_rootkit);
+module_exit(cleanup_rootkit);
