f61603b1c4b537e7574953c6d8d1fc99e35975e8
Pedro Peck <pedrop.peck@gmail.com>
2020-06-08 09:10:04 +0300

    Remove function get_syscall_table since it's not working. New version of function needs to be written.

--- rootkit.c
+++ rootkit.c
@@ -1,21 +1,24 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/syscalls.h>
+#include <asm/unistd.h>
+#include <linux/sched.h>
+#include <linux/dirent.h>
+#include <linux/slab.h>
+#include <linux/version.h>
+#include <asm-generic/unistd.h>
 
-static unsigned long int get_syscall_table(void)
+static unsigned long * get_syscall_table(void)
 {
-    unsigned long *sys_call_table;
-    unsigned long int i;
-
-    for (i = sys_close; i < ULONG_MAX; i += sizeof(void *))
-        if (sys_call_table[__NR_close] == (unsigned long)sys_close)
-            return sys_call_table;
-    return NULL;
+    
 }
 
 static int __init init_rootkit(void)
 {
     printk(KERN_INFO "%p\n", get_syscall_table);
+
+    return 0;
 }
 
 static void __exit cleanup_rootkit(void)
