bc56060de1ecc2d580e0b8555f12f9c0a07c5b3c
Pedro Peck <pedrop.peck@gmail.com>
2020-07-08 17:14:51 +0300

    Add tests.

---
+++ tests/hook_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_LEN 32
+
+int main()
+{
+    char buffer[BUF_LEN] = { 0 };
+    int fd;
+    
+    if ((fd = open("./test_file", O_RDWR)) >= 0)
+    {
+        read(fd, buffer, BUF_LEN);
+        puts(buffer);
+        close(fd);
+    }
+    else
+    {
+        puts("File not found...");
+    }
+
+    return 0;
+}
